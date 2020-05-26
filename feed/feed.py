f9738e29a25bdf48d417e35f5d7b2383aa66de92
martina-zxy <martina.megasari@gmail.com>
2020-05-26 23:41:15 +0200

    Undo a renaming error

--- feed/feed.py
+++ feed/feed.py
@@ -24,12 +24,12 @@ def transform_av_response_to_candles(av_data, av_meta_data) -> list:
     df_data = pd.DataFrame.from_dict(av_data, orient='index')
     df_data.columns = ['open', 'high', 'low', 'close', 'volume']
     df_data['symbol'] = settings.SYMBOL
-    df_data['__interval'] = av_meta_data['4. Interval']
+    df_data['interval'] = av_meta_data['4. Interval']
     df_data.index = pd.to_datetime(df_data.index)
     df_data.index = df_data.index.tz_localize(av_meta_data['6. Time Zone']).tz_convert('Europe/Paris')
     df_data.index = df_data.index.rename('timestamp')
     df_data = df_data.reset_index()
-    df_data = df_data[['timestamp', 'symbol', '__interval', 'open', 'high', 'low', 'close', 'volume']]
+    df_data = df_data[['timestamp', 'symbol', 'interval', 'open', 'high', 'low', 'close', 'volume']]
     return list(df_data.T.to_dict().values())
 
 
@@ -39,7 +39,7 @@ def insert_candles_to_db(l_candle) -> list:
     for candle in l_candle:
         key = {'timestamp': candle['timestamp'],
                'symbol': candle['symbol'],
-               '__interval': candle['__interval']}
+               'interval': candle['interval']}
         res = db['candles'].update(key, candle, upsert=True)
         if not res['updatedExisting']:
             l_new_candle.append(candle)
